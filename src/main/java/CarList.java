fa91be05e708c58a08e7e164402f19fed3831349
Naumovets <naumovets.daniil2003@mail.ru>
2023-01-26 02:41:03 +0300

    Added new collection - LinkedList and update jUnit tests for LinkedList

--- src/main/java/CarList.java
+++ src/main/java/CarList.java
@@ -1,10 +1,10 @@
 public interface CarList {
     void add(Car car, int index);
+    void add(Car car);
     int size();
     boolean remove(Car car);
     boolean removeAt(int index);
     void clear();
-    void add(Car car);
     Car get(int index);
 
 
